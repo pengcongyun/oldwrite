120e477afe7ea8c4916af0a321b2a714193472bc
pengcongyun <412027010@qq.com>
2017-07-24 09:28:17 +0800

    视频

--- Vquery/index.php
+++ Vquery/index.php
@@ -1,12 +1,12 @@
 <?php
 require_once ('Vquery.php');
 $arr=array(
-    "url"=>'http://www.bazhuayu.com',
+    "url"=>'http://www.yuntask.com',
     'method'=>'get'or 'post',
     'data'=>array('username'=>'admin','password'=>'admin'),
     'header'=>array('DESC:admin',),
 		);
 
 $cc=new Vquery($arr);
-$s=$cc->find('爱情');
+$s=$cc->find('a')->html();
 var_dump($s);
