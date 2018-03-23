600f36ef1bab01658d5de86fab27597a7bdb26f9
pengcongyun <412027010@qq.com>
2018-03-23 17:36:50 +0800

    qf

--- phpajaxpage/ajax/data.php
+++ phpajaxpage/ajax/data.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("127.0.0.1",'root','root','yii_quanshen_new')or die('error');
+$conn=mysqli_connect("127.0.0.1",'root','root','yii_quanshen_test')or die('error');
 mysqli_query($conn,'set names utf8');
 $sql="select * from article limit 1,5";
 $data=mysqli_query($conn,$sql);
--- phpajaxpage/ajaxla/data.php
+++ phpajaxpage/ajaxla/data.php
@@ -1,8 +1,9 @@
 <?php
-$conn=mysqli_connect("127.0.0.1",'root','root','yii_quanshen_new')or die('error');
+$conn=mysqli_connect("127.0.0.1",'root','root','yii_quanshen_test')or die('error');
 mysqli_query($conn,'set names utf8');
 $ty=$_POST['type'];
-$sql="select * from article limit $ty,5";
+$type=($ty-1)*100;
+$sql="select * from article limit $type,100";
 $data=mysqli_query($conn,$sql);
 foreach ($data as $i=>$da){
     $tt[$i][]=$da['article_id'];
