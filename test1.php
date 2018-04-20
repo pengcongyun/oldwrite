a1767563cf20e8451e965ff10feec44b862c98dc
pengcongyun <412027010@qq.com>
2018-04-20 17:45:48 +0800

    qf

---
+++ test1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/19 0019
+ * Time: 15:35
+ */
+//$arr=[1,2,'你好'];
+$arr=[1,2,3];
+foreach ($arr as $a){
+    if(!is_numeric($a)){
+        echo '不全是数字';exit;
+    }
+}
+echo '全是数字';exit;
