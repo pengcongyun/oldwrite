5fd112ffc87e6f86415e40db2173b29d20df050a
pengcongyun <412027010@qq.com>
2018-08-16 16:59:51 +0800

    qf

--- test1.php
+++ test1.php
@@ -1,17 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/4/19 0019
- * Time: 15:35
- */
-//$arr=[1,2,'你好'];
-//$arr=[1,2,3];
-//foreach ($arr as $a){
-//    if(!is_numeric($a)){
-//        echo '不全是数字';exit;
-//    }
-//}
-//echo '全是数字';exit;
-$data=['id'=>22,'msg'=>33];
-echo json_encode($data);exit;
+$arr=explode('.','1.00')[1];
+var_dump($arr);
