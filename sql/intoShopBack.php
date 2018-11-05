64864f69eeaa3dafcc97dcfc0aa5ca3a2ee44d05
pengcongyun <412027010@qq.com>
2018-11-05 17:24:43 +0800

    qf

--- sql/intoShopBack.php
+++ sql/intoShopBack.php
@@ -5,20 +5,21 @@ $conn=mysqli_connect("127.0.0.1",'root','root','yii_niuniu')or die('error');
 mysqli_query($conn,'set names utf8');
 $sql="select * from `shop_product_back_empty` where shop_id=50";
 $stmt=mysqli_query($conn,$sql);//执行sql查询语句
-
-//$ids=[168,169,170,171,172];
-//$ids=[173,174,175,176,177];
-//$ids=[178,179,180];
-//3-50-214
-for ($i=51;$i<214;$i++){
+for ($i=51;$i<258;$i++){
     foreach ($stmt as $row){
-        $back_empty_name=$row['back_empty_name'];
-        $back_empty_real_price=$row['back_empty_real_price'];
-        $product_id=$row['product_id'];
-        $shop_id=$i;
-        $in_sql="insert into `shop_product_back_empty` (back_empty_name, back_empty_real_price,product_id,shop_id) values
+        $same_sql='select * from `shop_product_back_empty` where shop_id='.$i.' and product_id='.$row['product_id'].' and back_empty_name='.$row['back_empty_name'];
+        $count=mysqli_num_rows(mysqli_query($conn,$same_sql));
+        if($count==0){
+            $back_empty_name=$row['back_empty_name'];
+            $back_empty_real_price=$row['back_empty_real_price'];
+            $product_id=$row['product_id'];
+            $shop_id=$i;
+            $in_sql="insert into `shop_product_back_empty` (back_empty_name, back_empty_real_price,product_id,shop_id) values
 ({$back_empty_name},{$back_empty_real_price},{$product_id},{$shop_id})";
-        mysqli_query($conn,$in_sql);
+            mysqli_query($conn,$in_sql);
+        }
     }
 }
 echo 'ok';exit;
+
+//http://www.old.com/sql/intoShopBack.php
---
+++ sql/shopProduct.php
@@ -0,0 +1,40 @@
+<?php
+// 输出Excel文件头
+header('Content-Type: application/vnd.ms-excel;charset=gbk');
+header('Content-Disposition: attachment;filename="'.date('Ymd').'.csv"');
+header('Cache-Control: max-age=0');
+// 从数据库中获取数据
+$conn=mysqli_connect("39.104.156.225",'root','WpFwf4LP','yii_niuniu')or die('error');
+mysqli_query($conn,'set names utf8');
+
+$sql='select sob.organization_brand_name,s.alias,c.category_name,pb.product_brand_name,p.product_name,concat(pde.capacity,(case when pde.capacity_unit=2 then "升" else "毫升" end)) as rj,(case when sp.order_method=2 then "单品" else "整件" end) as order_method,p.default_price,sp.settlement_price from shop_product sp join shop s on sp.shop_id=s.shop_id join shop_organization_brand sob on s.shop_organization_brand_id=sob.shop_organization_brand_id join product p on sp.product_id=p.product_id join category c on c.category_id=p.category_id join product_brand pb on p.product_brand_id=pb.product_brand_id join product_description pde on p.product_id=pde.product_id where sp.shop_product_id>1 and sob.shop_organization_brand_id in (2,3,4,5,6,7,8,9,10,11,12,13) order by sob.shop_organization_brand_id asc';
+$stmt=mysqli_query($conn,$sql);
+// PHP文件句柄，php://output 表示直接输出到浏览器
+$fp = fopen('php://output', 'a');
+// 输出Excel列头信息
+$head = array('商铺品牌', '店铺名称', '商品类型', '商品品牌', '商品名称', '商品规格', '订购方式', '标准单价', '结算单价');
+foreach ($head as $i => $v) {
+    // CSV的Excel支持GBK编码，一定要转换，否则乱码
+    $head[$i] = iconv('utf-8', 'gbk', $v);
+}
+// 写入列头
+fputcsv($fp, $head);
+// 计数器
+$cnt = 0;
+// 每隔$limit行，刷新一下输出buffer，节约资源
+$limit = 100;
+// 逐行取出数据，节约内存
+while ($row = mysqli_fetch_row($stmt)) {
+    $cnt ++;
+    if ($limit == $cnt) { //刷新一下输出buffer，防止由于数据过多造成问题
+        ob_flush();
+        flush();
+        $cnt = 0;
+    }
+    foreach ($row as $i => $v) {
+        $row[$i] = iconv('utf-8', 'gbk', $v);
+    }
+    fputcsv($fp, $row);
+}
+// 导出商铺 商品详情
+//http://www.old.com/sql/shopProduct.php
