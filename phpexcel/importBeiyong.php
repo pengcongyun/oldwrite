8d46b5950c64e9f54a7b7d576667ac5a93c54b47
pengcongyun <412027010@qq.com>
2019-01-18 19:03:16 +0800

    qf

--- phpexcel/importBeiyong.php
+++ phpexcel/importBeiyong.php
@@ -8,14 +8,14 @@
  */
 //连接数据库
 //$conn=mysqli_connect("127.0.0.1",'root','root','yii2_niuniu')or die('error');
-$conn = mysqli_connect("39.104.156.225", 'root', 'WpFwf4LP', 'yii_niuniu') or die('error');
+//$conn = mysqli_connect("39.104.156.225", 'root', 'WpFwf4LP', 'yii_niuniu') or die('error');
 mysqli_query($conn,'set names utf8');
 require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
-//$objReader = PHPExcel_IOFactory::createReader('Excel5');//use excel2007 for 2007 format
-$objReader = PHPExcel_IOFactory::createReader('Excel2007');//use excel2007 for 2007 format
+$objReader = PHPExcel_IOFactory::createReader('Excel5');//use excel2007 for 2007 format
+//$objReader = PHPExcel_IOFactory::createReader('Excel2007');//use excel2007 for 2007 format
 $count=0;
-//$objPHPExcel = $objReader->load('./resource/269.xls'); //$filename可以是上传的表格，或者是指定的表格
-$objPHPExcel = $objReader->load('./resource/300.xlsx'); //$filename可以是上传的表格，或者是指定的表格
+$objPHPExcel = $objReader->load('./resource/307.xls'); //$filename可以是上传的表格，或者是指定的表格
+//$objPHPExcel = $objReader->load('./resource/300.xlsx'); //$filename可以是上传的表格，或者是指定的表格
 $sheet = $objPHPExcel->getSheet(0);
 $highestRow = $sheet->getHighestRow(); // 取得总行数
     // $highestColumn = $sheet->getHighestColumn(); // 取得总列数
@@ -24,27 +24,25 @@ $highestRow = $sheet->getHighestRow(); // 取得总行数
     //$a表示列号 216-234
 //for($shop_id=216;$shop_id<=234;$shop_id++){
     for($i=2;$i<=$highestRow;$i++){
-        $a = $objPHPExcel->getActiveSheet()->getCell("I".$i)->getValue();//获取订购价格
-        $b = $objPHPExcel->getActiveSheet()->getCell("J".$i)->getValue();//获取结算价格
-        $c = $objPHPExcel->getActiveSheet()->getCell("D".$i)->getValue();//product_id
-        $d = 300;//商铺ID
-        $e = $objPHPExcel->getActiveSheet()->getCell("G".$i)->getValue();//订购方式
-        $sql="select * from `product_price` where product_price_id=".$c;
-        $stmt=mysqli_query($conn,$sql);//执行sql查询语句
-        foreach ($stmt as $row){
-            $number_per_box=$row['number_per_box'];
-            $product_price_id=$row['product_price_id'];
-            $in_sql = "insert into `shop_product` (order_price,settlement_price,order_method,number_per_box,product_id,product_price_id,shop_id) values
-({$a},{$b},{$e},{$number_per_box},{$c},{$product_price_id},{$d})";
-            mysqli_query($conn, $in_sql);
-            $count++;
-//            if ($res) {
-//                echo $i."添加成功！";
-//            }else{
-//                echo "添加失败！";
-//                exit();
-//            }
+        $a = $objPHPExcel->getActiveSheet()->getCell("J".$i)->getValue();//获取订购价格
+        $b = $objPHPExcel->getActiveSheet()->getCell("K".$i)->getValue();//获取结算价格
+        $c = $objPHPExcel->getActiveSheet()->getCell("H".$i)->getValue();//product_price_id
+        $d = $objPHPExcel->getActiveSheet()->getCell("G".$i)->getValue();//product_id
+        $e = $objPHPExcel->getActiveSheet()->getCell("F".$i)->getValue();//每件数据
+        $f = 307;//商铺ID
+        $g = $objPHPExcel->getActiveSheet()->getCell("E".$i)->getValue();//订购方式
+
+        $in_sql = "insert into `shop_product` (order_price,settlement_price,order_method,number_per_box,product_id,product_price_id,shop_id) values
+({$a},{$b},{$g},{$e},{$d},{$c},{$f})";
+        $res=mysqli_query($conn, $in_sql);
+        $count++;
+        if ($res) {
+            echo $i."添加成功！";
+        }else{
+            echo "添加失败！";
+            exit();
         }
+
     }
 //}
 echo 'ok';exit;
--- sql/intoShopProduct.php
+++ sql/intoShopProduct.php
@@ -2,10 +2,10 @@
 //商铺商品 导入到 新商铺
 header("Content-Type: text/html; charset=utf-8");
 //$conn=mysqli_connect("127.0.0.1",'root','root','yii2_niuniu')or die('error');
-$conn=mysqli_connect("39.104.156.225",'root','WpFwf4LP','yii_niuniu')or die('error');
+//$conn=mysqli_connect("39.104.156.225",'root','WpFwf4LP','yii_niuniu')or die('error');
 mysqli_query($conn,'set names utf8');
-$i=297;
-$sql="select * from `shop_product` where shop_id=50";
+//$i=297;
+$sql="select * from `shop_product` where shop_id=301";
 $stmt=mysqli_query($conn,$sql);//执行sql查询语句
 //$ids=[168,169,170,171,172];
 //$ids=[173,174,175,176,177];
@@ -13,7 +13,7 @@ $stmt=mysqli_query($conn,$sql);//执行sql查询语句
 //$ids=[185,214];
 //$ids=[215,240];
 //     257
-//for ($i=263;$i<=264;$i++){
+for ($i=302;$i<=303;$i++){
     foreach ($stmt as $row){
         $order_method=$row['order_method'];
         $number_per_box=$row['number_per_box'];
@@ -26,6 +26,6 @@ $stmt=mysqli_query($conn,$sql);//执行sql查询语句
 ({$order_method},{$number_per_box},{$order_price},{$price},{$product_id},{$product_price_id},{$shop_id})";
         mysqli_query($conn,$in_sql);
     }
-//}
+}
 echo 'ok';exit;
 //http://www.old.com/sql/intoShopProduct.php
