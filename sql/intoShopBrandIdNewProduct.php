d7f306dad84c91e57b7825c91a78bdefddfa5eb0
pengcongyun <412027010@qq.com>
2019-01-21 22:27:30 +0800

    qf

--- sql/intoShopBrandIdNewProduct.php
+++ sql/intoShopBrandIdNewProduct.php
@@ -8,10 +8,10 @@
 //新商品 导入到 各大品牌商铺
 header("Content-Type: text/html; charset=utf-8");
 //$conn=mysqli_connect("39.104.156.225",'root','WpFwf4LP','yii_niuniu')or die('error');
-$conn=mysqli_connect("127.0.0.1",'root','root','yii_niuniu')or die('error');
+//$conn=mysqli_connect("127.0.0.1",'root','root','pp')or die('error');
 mysqli_query($conn,'set names utf8');
-$new_product_id=[142];
-$shop_brand_ids=[101];
+$new_product_id=[15];
+$shop_brand_ids=[26,23];
 $shop_ids=[];
 foreach ($shop_brand_ids as $shop_brand_id){
     $sqls='select shop_id from shop where shop_organization_brand_id='.$shop_brand_id.' order by shop_id asc';
@@ -20,18 +20,24 @@ foreach ($shop_brand_ids as $shop_brand_id){
         $shop_ids[]=$v['shop_id'];
     }
 }
+//var_dump($shop_ids);exit;
 foreach ($new_product_id as $id){
-    $sql_product='select * from `product` where product_id='.$id;
+    $sql_product='select * from `product_price` where product_price_id='.$id;
     $row=mysqli_fetch_assoc(mysqli_query($conn,$sql_product));
     foreach ($shop_ids as $shop){
-        $price=$row['default_price'];
-        $order_price=$row['default_price'];
-        $order_method=$row['order_method'];
-        $product_id=$row['product_id'];
-        $shop_id=$shop;
-        $in_sql="insert into `shop_product` (order_price,settlement_price,order_method,product_id,shop_id) values
-({$order_price},{$price},{$order_method},{$product_id},{$shop_id})";
-        mysqli_query($conn,$in_sql);
+        $sql_exist='select * from shop_product where product_price_id='.$id.' and shop_id='.$shop;
+        $count=mysqli_num_rows(mysqli_query($conn,$sql_exist));
+        if($count==0){
+            $price = 43;
+            $number_per_box = $row['number_per_box'];
+            $order_method = $row['order_method'];
+            $product_price_id = $row['product_price_id'];
+            $product_id = $row['product_id'];
+            $shop_id = $shop;
+            $in_sql = "insert into `shop_product` (order_price,settlement_price,order_method,number_per_box,product_id,product_price_id,shop_id) values
+({$price},{$price},{$order_method},{$number_per_box},{$product_id},{$product_price_id},{$shop_id})";
+            mysqli_query($conn, $in_sql);
+        }
     }
 }
 echo '新商品ok';exit;
