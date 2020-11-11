80b06527c9cb8d3f50a60dde0869d600faa0e3a3
pengcongyun <412027010@qq.com>
2020-11-11 14:38:44 +0800

    1

--- dd.php
+++ dd.php
@@ -1,19 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: CleverCloud
- * Date: 2019/1/18
- * Time: 10:36
- */
-//$conn=mysqli_connect("39.104.156.225",'root','WpFwf4LP','yii_niuniu')or die('error');
-//mysqli_query($conn,'set names utf8');
-//$sql="select shop_id from shop_product where shop_product_id>1";
-//$stmt=mysqli_query($conn,$sql);//执行sql查询语句
-//$ids=[];
-//foreach ($stmt as $k){
-//    $ids[]=$k['shop_product_id'];
-//}
-//'570,236,294,114,573,578,448,425,467,135,136,453,149,90,567,168,179,188,564,584,595,145,20,556,557,139,582,251,535,569,562,447'
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="http://api.newniu.com/heshi/order/order?signature=23" method="post">
+    <input type="text" name="body">
 
-unlink('D:/phpStudy/WWW/yii2_niuniu/images/data/test1.csv');
-echo 11;
+    <input type="submit">
+</form>
+</body>
+</html>
